6e32dc461526083b857f9da0f38802c3e0908d15
envincebal <vincentyan1@gmail.com>
2022-05-11 16:12:35 -0700

    Added component folders.

--- src/App.jsx
+++ src/App.jsx
@@ -12,36 +12,42 @@ class App extends Component {
     console.log("constructor")
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(res => this.setState(() => {
-      return {monsters: res}
-    }, () => {
-      console.log(this.state);
-    }))
+      .then(res => res.json())
+      .then(res => this.setState(() => {
+        return {monsters: res}
+      }, () => {
+        console.log(this.state);
+      }))
     console.log("componentDidMount")
   }
 
-  render() {      
-    const filteredMonsters = this.state.monsters.filter(monster => monster.name.toLocaleLowerCase().includes(this.state.searchField))
+  onSearchChange = (e) => {
+    const searchField = e
+    .target
+    .value
+    .toLocaleLowerCase();
+  this.setState({searchField})
+  }
+
+  render() {
+    const {monsters,searchField} = this.state;
+    const {onSearchChange}  = this;
+    const filteredMonsters = monsters
+      .filter(monster => monster.name.toLocaleLowerCase().includes(searchField))
     return (
       <div className="App">
-    { console.log("render")}
-    <input className='search-box' type="search" placeholder="search monsters" onChange={(e) => {
-      const searchField = e.target.value.toLocaleLowerCase();
-
-      
-      this.setState( {
-        searchField
-      })
-      }} />
-        {filteredMonsters
-          .map(monster => {
-            return <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          })}
+        {console.log("render")}
+        <input
+          className='search-box'
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}/> {filteredMonsters.map(monster => {
+          return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        })}
       </div>
     )
   }
